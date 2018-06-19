10565512f29f2e31a7a6c7d1e24979a7d9c0c3f8
tjogrady1982 <38034744+tjogrady1982@users.noreply.github.com>
2018-06-19 19:34:53 +0100

    Add files via upload

---
+++ Locking/Locking/DataStorage.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Locking
+{
+    class DataStorage
+    {
+        private class DataStore { public int Value { get; set; } }
+
+        private DataStore store = new DataStore();
+        private DataStore extraStore = new DataStore();
+
+        public void ConcurrencyTest()
+        {
+            var thread1 = new Thread(IncrementTheValue);
+            var thread2 = new Thread(SubtractTheValue);
+            var thread3 = new Thread(IncrementTheValue);
+            var thread4 = new Thread(SubtractTheValue);
+
+
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+            thread4.Start();
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+            thread4.Join();
+
+
+            Console.WriteLine($"Final value: {store.Value}");
+            Console.WriteLine($"Final value: {extraStore.Value}");
+        }
+
+        public void DeadlockTest()
+        {
+            var thread3 = new Thread(SubtractTheValue);
+            var thread4 = new Thread(SubtractTheValue);
+
+            thread3.Start();
+            thread4.Start();
+
+            thread3.Join();
+            thread4.Join();
+
+            Console.WriteLine($"Final value: {extraStore.Value}");
+        }
+
+        private void IncrementTheValue()
+        {
+            lock (extraStore)
+            {
+
+
+                lock (store)
+                {
+                    extraStore.Value++;
+                    store.Value++;
+                }
+            }
+        }
+
+        private void SubtractTheValue()
+        {
+            lock (store)
+            {
+
+
+
+                lock (extraStore)
+                {
+                    extraStore.Value--;
+                    store.Value--;
+                }
+            }
+
+        }
+    }
+}
---
+++ Locking/Locking/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var incremental = new DataStorage();
+            var deadLock = new DataStorage();
+
+            incremental.ConcurrencyTest();
+            deadLock.DeadlockTest();
+
+
+            Console.ReadLine();
+        }
+    }
+}
